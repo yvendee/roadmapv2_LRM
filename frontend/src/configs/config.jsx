02fe86776b56235767031d92476d4d4e3eed56b6
yvendee <yvendee2020@gmail.com>
2025-08-27 20:36:30 +0800

    update

--- frontend/src/configs/config.jsx
+++ frontend/src/configs/config.jsx
@@ -1,7 +1,7 @@
 // frontend\src\configs\config.jsx
-const API_URL = 'http://localhost:80/api';    // for development
+// const API_URL = 'http://localhost:80/api';    // for development
 
-// const API_URL = 'http://104.248.11.135/api';     // for production
+const API_URL = 'http://104.248.11.135/api';     // for production
 
 
 export const ENABLE_CONSOLE_LOGS = true; 
