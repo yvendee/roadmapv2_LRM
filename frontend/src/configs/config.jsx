965fcc4de3406669a33a336609561138b4b4c185
yvendee <yvendee2020@gmail.com>
2025-09-16 20:45:02 +0800

    update

--- frontend/src/configs/config.jsx
+++ frontend/src/configs/config.jsx
@@ -1,7 +1,9 @@
 // frontend\src\configs\config.jsx
 // const API_URL = 'http://localhost:80/api';    // for development
 
-const API_URL = 'http://104.248.11.135/api';     // for production
+// const API_URL = 'http://104.248.11.135/api'; 
+
+const API_URL = 'http://getmomentumos.com/api'; // for production
 
 
 export const ENABLE_CONSOLE_LOGS = true; 
