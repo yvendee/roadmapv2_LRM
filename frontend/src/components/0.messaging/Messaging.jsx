b7561b88bbc416fb10aebaff9ceb1033b98259c0
yvendee <yvendee2020@gmail.com>
2025-09-27 14:21:22 +0800

    update

--- frontend/src/components/0.messaging/Messaging.jsx
+++ frontend/src/components/0.messaging/Messaging.jsx
@@ -138,7 +138,7 @@ const ChatInterface = () => {
         const result = await response.json();
   
         if (response.ok) {
-          // addMessage(messageData);
+          addMessage(messageData);
           console.log('Message sent successfully:', result);
           setInput(""); // Clear the input field after sending the message
           // You can update your UI here with the new message data if needed
