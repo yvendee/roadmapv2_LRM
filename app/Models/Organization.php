89eeadad234c4ed098ba70cc24607231a9e01724
yvendee <yvendee2020@gmail.com>
2025-10-01 22:45:01 +0800

    update

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -24,88 +24,88 @@ class Organization extends Model
     {
         parent::boot();
 
-        // // Automatically generate UUID when creating
-        // static::creating(function ($model) {
-        //     $model->u_id = (string) Str::uuid();
-        // });
-
+        // Automatically generate UUID when creating
         static::creating(function ($model) {
-            $model->u_id = 'f5f9c6a3-f52b-410d-abf8-44f5459120c0';
+            $model->u_id = (string) Str::uuid();
         });
+
+        // static::creating(function ($model) {
+        //     $model->u_id = 'f5f9c6a3-f52b-410d-abf8-44f5459120c0';
+        // });
 
         // Automatically create related records after organization creation
         static::created(function ($organization) {
 
-            // // 🧩 Layout settings
-            // \App\Models\OpspLayoutSetting::create([
-            //     'u_id' => $organization->u_id,
-            //     'organizationName' => $organization->organizationName,
-            //     'strategicDriversStatus' => 'true',
-            //     'FoundationsStatus' => 'true',
-            //     'threeYearOutlookStatus' => 'true',
-            //     'playingToWinStatus' => 'true',
-            //     'coreCapabilitiesStatus' => 'true',
-            //     'fourDecisionsStatus' => 'true',
-            //     'ConstraintsTrackerStatus' => 'true',
-            //     'modifiedByEmail' => null,
-            //     'statusFlag' => null,
-            // ]);
+            // 🧩 Layout settings
+            \App\Models\OpspLayoutSetting::create([
+                'u_id' => $organization->u_id,
+                'organizationName' => $organization->organizationName,
+                'strategicDriversStatus' => 'true',
+                'FoundationsStatus' => 'true',
+                'threeYearOutlookStatus' => 'true',
+                'playingToWinStatus' => 'true',
+                'coreCapabilitiesStatus' => 'true',
+                'fourDecisionsStatus' => 'true',
+                'ConstraintsTrackerStatus' => 'true',
+                'modifiedByEmail' => null,
+                'statusFlag' => null,
+            ]);
 
-            // // 🆕 Strategic Drivers
-            // \App\Models\OpspStrategicDriver::create([
-            //     'u_id' => $organization->u_id,
-            //     'organizationName' => $organization->organizationName,
-            //     'strategicDriversData' => [],
-            //     'statusFlag' => null,
-            // ]);
+            // 🆕 Strategic Drivers
+            \App\Models\OpspStrategicDriver::create([
+                'u_id' => $organization->u_id,
+                'organizationName' => $organization->organizationName,
+                'strategicDriversData' => [],
+                'statusFlag' => null,
+            ]);
 
-            // // 🆕 Foundations
-            // \App\Models\OpspFoundation::create([
-            //     'u_id' => $organization->u_id,
-            //     'organizationName' => $organization->organizationName,
-            //     'foundationsData' => [],
-            //     'statusFlag' => null,
-            // ]);
+            // 🆕 Foundations
+            \App\Models\OpspFoundation::create([
+                'u_id' => $organization->u_id,
+                'organizationName' => $organization->organizationName,
+                'foundationsData' => [],
+                'statusFlag' => null,
+            ]);
 
-            // // 🆕 Three Year Outlook
-            // \App\Models\OpspThreeyearOutlook::create([
-            //     'u_id' => $organization->u_id,
-            //     'organizationName' => $organization->organizationName,
-            //     'threeyearOutlookData' => [],
-            //     'statusFlag' => null,
-            // ]);
+            // 🆕 Three Year Outlook
+            \App\Models\OpspThreeyearOutlook::create([
+                'u_id' => $organization->u_id,
+                'organizationName' => $organization->organizationName,
+                'threeyearOutlookData' => [],
+                'statusFlag' => null,
+            ]);
 
-            // // 🆕 Playing to Win Strategy
-            // \App\Models\OpspPlayingtowinStrategy::create([
-            //     'u_id' => $organization->u_id,
-            //     'organizationName' => $organization->organizationName,
-            //     'playingToWinStrategyData' => null,
-            //     'statusFlag' => null,
-            // ]);
+            // 🆕 Playing to Win Strategy
+            \App\Models\OpspPlayingtowinStrategy::create([
+                'u_id' => $organization->u_id,
+                'organizationName' => $organization->organizationName,
+                'playingToWinStrategyData' => null,
+                'statusFlag' => null,
+            ]);
 
-            // // 🆕 Core Capabilities
-            // \App\Models\OpspCoreCapability::create([
-            //     'u_id' => $organization->u_id,
-            //     'organizationName' => $organization->organizationName,
-            //     'coreCapabilitiesData' => [],
-            //     'statusFlag' => null,
-            // ]);
+            // 🆕 Core Capabilities
+            \App\Models\OpspCoreCapability::create([
+                'u_id' => $organization->u_id,
+                'organizationName' => $organization->organizationName,
+                'coreCapabilitiesData' => [],
+                'statusFlag' => null,
+            ]);
 
-            // // 🆕 Four Decisions
-            // \App\Models\OpspFourDecision::create([
-            //     'u_id' => $organization->u_id,
-            //     'organizationName' => $organization->organizationName,
-            //     'fourDecisionsData' => [],
-            //     'statusFlag' => null,
-            // ]);
+            // 🆕 Four Decisions
+            \App\Models\OpspFourDecision::create([
+                'u_id' => $organization->u_id,
+                'organizationName' => $organization->organizationName,
+                'fourDecisionsData' => [],
+                'statusFlag' => null,
+            ]);
 
-            // // 🆕 Constraints Tracker
-            // \App\Models\OpspConstraintsTracker::create([
-            //     'u_id' => $organization->u_id,
-            //     'organizationName' => $organization->organizationName,
-            //     'constraintsTrackerData' => [],
-            //     'statusFlag' => null,
-            // ]);
+            // 🆕 Constraints Tracker
+            \App\Models\OpspConstraintsTracker::create([
+                'u_id' => $organization->u_id,
+                'organizationName' => $organization->organizationName,
+                'constraintsTrackerData' => [],
+                'statusFlag' => null,
+            ]);
 
             // 🆕 Flywheel
             \App\Models\Flywheel::create([
@@ -115,29 +115,29 @@ class Organization extends Model
                 'statusFlag' => null,
             ]);
 
-            // // 🆕 Scoreboard: Annual Priorities
-            // \App\Models\ScoreboardAnnualpriority::create([
-            //     'u_id' => $organization->u_id,
-            //     'organizationName' => $organization->organizationName,
-            //     'annualPrioritiesdData' => [],
-            //     'statusFlag' => null,
-            // ]);
+            // 🆕 Scoreboard: Annual Priorities
+            \App\Models\ScoreboardAnnualpriority::create([
+                'u_id' => $organization->u_id,
+                'organizationName' => $organization->organizationName,
+                'annualPrioritiesdData' => [],
+                'statusFlag' => null,
+            ]);
 
-            // // 🆕 Scoreboard: Company Traction Card
-            // \App\Models\ScoreboardCompanyTractionCard::create([
-            //     'u_id' => $organization->u_id,
-            //     'organizationName' => $organization->organizationName,
-            //     'companyTractionCardData' => [],
-            //     'statusFlag' => null,
-            // ]);
+            // 🆕 Scoreboard: Company Traction Card
+            \App\Models\ScoreboardCompanyTractionCard::create([
+                'u_id' => $organization->u_id,
+                'organizationName' => $organization->organizationName,
+                'companyTractionCardData' => [],
+                'statusFlag' => null,
+            ]);
 
-            // // 🆕 Scoreboard: Project Progress Card
-            // \App\Models\ScoreboardProjectProgressCard::create([
-            //     'u_id' => $organization->u_id,
-            //     'organizationName' => $organization->organizationName,
-            //     'projectProgressCardData' => [],
-            //     'statusFlag' => null,
-            // ]);
+            // 🆕 Scoreboard: Project Progress Card
+            \App\Models\ScoreboardProjectProgressCard::create([
+                'u_id' => $organization->u_id,
+                'organizationName' => $organization->organizationName,
+                'projectProgressCardData' => [],
+                'statusFlag' => null,
+            ]);
 
             // 🆕 GCC: Metrics
             \App\Models\GccMetric::create([
