5b0eaabe11314beea31a82be8b5a5e26b7093a54
yvendee <yvendee2020@gmail.com>
2025-10-01 22:06:20 +0800

    update

--- database/seeders/OpspPlayingtowinStrategySeeder.php
+++ database/seeders/OpspPlayingtowinStrategySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\OpspPlayingtowinStrategy;
+use Illuminate\Support\Str;
 
 class OpspPlayingtowinStrategySeeder extends Seeder
 {
@@ -28,7 +29,7 @@ class OpspPlayingtowinStrategySeeder extends Seeder
         ];
 
         OpspPlayingtowinStrategy::create([
-            'u_id' => 1,
+            'u_id' => Str::uuid(),
             'organizationName' => 'Chuck Gulledge Advisors, LLC',
             'playingToWinStrategyData' => json_encode($data),
             'statusFlag' => null,
