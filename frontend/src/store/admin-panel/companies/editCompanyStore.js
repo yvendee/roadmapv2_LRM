be2e170bff27f597063ec89a187b40d9ce89a6f3
yvendee <yvendee2020@gmail.com>
2025-10-09 21:01:39 +0800

    update

--- frontend/src/store/admin-panel/companies/editCompanyStore.js
+++ frontend/src/store/admin-panel/companies/editCompanyStore.js
@@ -2,8 +2,21 @@
 // frontend\src\store\admin-panel\companies\editCompanyStore.js
 import { create } from 'zustand';
 
+// export const useEditCompanyStore = create((set) => ({
+//   name: 'eDoc Innovations',
+//   quarters: {
+//     Q1: ['January', 'February'],
+//     Q2: ['April', 'May', 'June'],
+//     Q3: ['July', 'August'],
+//     Q4: ['October', 'November'],
+//   },
+//   setName: (name) => set({ name }),
+//   setQuarters: (quarters) => set({ quarters }),
+// }));
+
+
 export const useEditCompanyStore = create((set) => ({
-  name: 'eDoc Innovations',
+  name: '',
   quarters: {
     Q1: ['January', 'February'],
     Q2: ['April', 'May', 'June'],
