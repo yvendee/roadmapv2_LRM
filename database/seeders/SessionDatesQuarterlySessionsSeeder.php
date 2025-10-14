3352500b91654a3f008157e32cbf0202e1731ee2
yvendee <yvendee2020@gmail.com>
2025-10-14 20:42:59 +0800

    update

--- database/seeders/SessionDatesQuarterlySessionsSeeder.php
+++ database/seeders/SessionDatesQuarterlySessionsSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\SessionDatesQuarterlySessions;
+use App\Models\SessionDatesQuarterlySession;
 use Illuminate\Support\Str;
 
 class SessionDatesQuarterlySessionsSeeder extends Seeder
